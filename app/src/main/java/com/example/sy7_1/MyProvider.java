1b0897e522fac0ab61f1a30a4ad23938d404878e
Kong-Kongg <104617753+Kong-Kongg@users.noreply.github.com>
2022-11-04 15:53:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sy7_1/MyProvider.java
@@ -0,0 +1,56 @@
+package com.example.sy7_1;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyProvider extends ContentProvider {
+    public static final int TABLE1_DIR = 0;
+    public static final int TABLE1_ITEM =1;
+    public static final String AUTHORITY = "com.example.SY7_2.provider";
+    private static final UriMatcher uriMatcher ;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.example.app.provider","table1",TABLE1_DIR);
+        uriMatcher.addURI("com.example.app.provider","table1/#",TABLE1_ITEM);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
